9ff08c3f82680cf6d52c5d3399f195c96cb48e83
LucasGuzylak <lucasguzy11@gmail.com>
2025-05-21 21:05:04 -0700

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,25 @@ function App() {
 
   return (
     <div>
-      <main>
-        
-        
-        <div className="newcontainer">
+        <div className="navbar bg-base-100 shadow-sm fixed top-1/30 left-1/2 -translate-x-1/2 w-1/3 z-50 flex justify-between rounded-full">
+          <a className="btn btn-soft btn-info rounded-full">Home</a>
+          <a className="btn btn-soft btn-info rounded-full">Projects</a>
+          <a className="btn btn-soft btn-info rounded-full">Languages</a>
+          <a className="btn btn-soft btn-info rounded-full">Experience</a>
+          <a className="btn btn-soft btn-info rounded-full">Academics</a>
+        </div>
+
+      <main className="min-h-screen pt-24">
+        <div className="newcontainer" style={{position: "relative"}}>
           <div className="hi-font">Hi! I'm</div>
           <div className="name-font">Lucas Guzylak</div>
+          <div className="avatar" style={{position: "absolute", right: 20}}>
+            <div className="w-24 rounded-full">
+            <img src="https://img.daisyui.com/images/profile/demo/yellingcat@192.webp" />
+            </div>
+          </div>
         </div>
+        
 
         <a href = "https://github.com/LucasGuzylak">
           <img src="/github-mark-white.png" alt="GitHub profile for LucasGuzylak"
@@ -26,7 +38,8 @@ function App() {
             My Projects
           </div>
         </div>
-      </main>
+
+        </main>
       <footer>
       </footer>
     </div>
