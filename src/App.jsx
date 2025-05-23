cc5078e0b11560e946abaea02e4b4ceadde2643d
LucasGuzylak <lucasguzy11@gmail.com>
2025-05-23 16:05:50 -0700

    Education Finished

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,13 @@ function App() {
 
   return (
     <div>
+        <div id="Home" style={{ paddingTop:'100px', marginTop:'-100px'}}></div>
         <div className="navbar bg-base-100 shadow-sm fixed top-1/30 left-1/2 -translate-x-1/2 w-1/3 z-50 flex justify-between rounded-full">
-          <a className="btn btn-soft btn-info rounded-full">Home</a>
-          <a className="btn btn-soft btn-info rounded-full">Projects</a>
-          <a className="btn btn-soft btn-info rounded-full">Languages</a>
-          <a className="btn btn-soft btn-info rounded-full">Experience</a>
-          <a className="btn btn-soft btn-info rounded-full">Academics</a>
+          <a href="#Home" className="btn btn-soft btn-info rounded-full h-[3rem]">Home</a>
+          <a href="#Projects" className="btn btn-soft btn-info rounded-full h-[3rem]">Projects</a>
+          <a href="#Skills" className="btn btn-soft btn-info rounded-full h-[3rem]">Skills</a>
+          <a href="#Experience" className="btn btn-soft btn-info rounded-full h-[3rem]">Experience</a>
+          <a href="#Education" className="btn btn-soft btn-info rounded-full h-[3rem]">Education</a>
         </div>
 
       <main className="min-h-screen pt-24">
@@ -33,15 +34,47 @@ function App() {
           <img src="/InBug-White.png" alt="LinkedIn profile for LucasGuzylak"
           width={50} height={50} style={{position: "fixed", bottom: 18, left: 100}}/>
         </a>
+
+        <div id="Projects" style={{height: '10px'}}></div>
+
         <div className="next-screen">
           <div className="my-projects">
             My Projects
           </div>
         </div>
 
+        <div id="Skills" className="next-screen" style={{marginTop: 0, borderTopLeftRadius: 0, borderTopRightRadius: 0}}>
+          <div className="my-projects">
+            My Skills
+          </div>
+        </div>
+
+        <div id="Experience" className="next-screen" style={{marginTop: 0, borderTopLeftRadius: 0, borderTopRightRadius: 0}}>
+          <div className="my-projects">
+            My Experience
+          </div>
+        </div>
+
+        <div id="Education" className="next-screen" style={{marginTop: 0, borderTopLeftRadius: 0, borderTopRightRadius: 0, paddingBottom: 50}}>
+          <div className="my-projects">
+            My Education
+          </div>
+          <div style={{marginTop: "50px"}}/>
+          <div className="flex justify-center items-center gap-10 mx-auto" style={{backgroundColor: "#3f90e0", maxWidth: "900px"}}>
+            <img src="/college-of-sciences-vert-1color-black-rgb.png"
+              style={{width: "300px", height: "auto", position: "absolute", left: "18%"}} 
+              alt="College of Sciences Logo"/>
+            <div className="text-left" style={{color: "black", fontSize: "30px", marginLeft: "400px"}}>
+              <h4>Pursuing a Bachelor of Science</h4>
+              <h5>San Diego State University</h5>
+              <h5>Graduating in 2028</h5>
+              <h5>GPA 3.8</h5>
+              <h5>Major in Computer Science</h5>
+              <h5>Dean's List - Fall 2024 & Spring 2025</h5>
+            </div>
+          </div>
+        </div>
         </main>
-      <footer>
-      </footer>
     </div>
   )
 }
